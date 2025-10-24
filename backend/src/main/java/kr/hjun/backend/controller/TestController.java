8f54f6b51a97e393f345acafca12f06db2763abc
hJun-KR <h0ngjunp.park@gmail.com>
2025-10-24 13:51:14 +0900

    chore: 기본 백엔드 상태 커밋

---
+++ backend/src/main/java/kr/hjun/backend/controller/TestController.java
@@ -0,0 +1,12 @@
+package kr.hjun.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello World";
+    }
+}
