5988071d6f6274c4a53e609b46b115a9d17ee5d4
hJun-KR <h0ngjunp.park@gmail.com>
2025-10-29 08:36:53 +0900

    feat: BaseEntitiy 및 UserEntitiy 추가

--- backend/src/main/java/kr/hjun/backend/entity/User.java
+++ backend/src/main/java/kr/hjun/backend/entity/User.java
@@ -1,4 +1,39 @@
 package kr.hjun.backend.entity;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 100)
+    private String password;
+
+    @Column(name = "name", nullable = false, length = 30)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 10)
+    private Role role;
+
+    @Column(name = "is_active", nullable = false)
+    private boolean isActive = true;
+
+    public enum Role {
+        USER, ADMIN
+    }
 }
